bf56b30ed0a33f919cd406f613096966b45d365a
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-04-27 14:25:32 -0300

    fix para ver se agora vai

--- components/YouTube.tsx
+++ components/YouTube.tsx
@@ -1,15 +1,14 @@
 // components/mdx/YouTube.tsx
 export default function YouTube({ id }: { id: string }) {
-    return (
-      <div>
-        <iframe
-          className="aspect-video w-full"
-          src={`https://www.youtube.com/embed/${id}`}
-          title="YouTube Video Player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <iframe
+        className="aspect-video w-full"
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube Video Player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  )
+}

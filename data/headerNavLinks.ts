8b8fbc8307dafae48e2fdcf9f2dcb95c34c9ebe3
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-04-26 22:58:39 -0300

    ajustes de nomes no nav bar e páginas

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,8 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/projetos', title: 'Projetos' },
+  { href: '/sobre', title: 'Sobre' },
 ]
 
 export default headerNavLinks

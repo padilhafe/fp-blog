8ca2b2def3edc759992ee43c5491b42649683465
Felipe Duellis Padilha <felipedpadilha@gmail.com>
2025-04-27 14:21:44 -0300

    fix para o prettier não reclamar

--- components/YouTube.tsx
+++ components/YouTube.tsx
@@ -1,13 +1,15 @@
-//components/mdx/YouTube.tsx
+// components/mdx/YouTube.tsx
 export default function YouTube({ id }: { id: string }) {
     return (
-        <div>
-            <iframe
-                className="aspect-video w-full"
-                src={"https://www.youtube.com/embed/" + id}
-                title="YouTube Video Player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-        </div>
+      <div>
+        <iframe
+          className="aspect-video w-full"
+          src={`https://www.youtube.com/embed/${id}`}
+          title="YouTube Video Player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
     );
-};
+  }
+  
